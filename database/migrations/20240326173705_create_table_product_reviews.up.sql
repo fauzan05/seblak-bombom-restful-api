7321e668d962d41a5aa6fd96689c6ce2fe4c7d60
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2024-03-27 05:57:10 +0700

    memperbaiki dan menambahkan tabel db, dan api-docs

--- database/migrations/20240326173705_create_table_product_reviews.up.sql
+++ database/migrations/20240326173705_create_table_product_reviews.up.sql
@@ -1,13 +1,12 @@
 CREATE TABLE product_reviews (
     id INTEGER AUTO_INCREMENT,
-    order_id INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     rate INTEGER NOT NULL,
+    comment TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (product_id) REFERENCES products (id),
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (order_id) REFERENCES orders (id)
+    FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 ) ENGINE = InnoDB;
