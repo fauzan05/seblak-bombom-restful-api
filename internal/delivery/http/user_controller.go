278fb6440fcf0454537f5401865de84d79f8e0eb
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2025-06-24 22:26:13 +0700

    Update user_controller.go

--- internal/delivery/http/user_controller.go
+++ internal/delivery/http/user_controller.go
@@ -187,7 +187,7 @@ func (c *UserController) Login(ctx *fiber.Ctx) error {
 		Secure:   false,                        // Harus HTTPS, matikan ini saat dev kalau perlu
 		SameSite: fiber.CookieSameSiteNoneMode, // Untuk cegah CSRF
 		Expires:  response.ExpiryDate,
-		Domain:   "seblak-bombom-restful-api-production.up.railway.app",
+		Domain:   "seblak-bombom-api-consumer-production.up.railway.app",
 	})
 
 	return ctx.Status(fiber.StatusOK).JSON(model.ApiResponse[*model.UserResponse]{
