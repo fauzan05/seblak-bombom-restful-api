4a4ceacc3851a4b35b1f397f7c0c5671d79f2615
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2025-06-25 10:20:39 +0700

    Update cookies_helper.go

--- internal/helper/cookies_helper/cookies_helper.go
+++ internal/helper/cookies_helper/cookies_helper.go
@@ -1,23 +1,52 @@
 package cookies_helper
 
 import (
-    "fmt"
-    "net/url"
-    "time"
-    "github.com/gofiber/fiber/v2"
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func SetCrossOriginCookie(ctx *fiber.Ctx, name, value string, expires time.Time) error {
-    // Format cookie dengan Partitioned attribute
+    origin := ctx.Get("Origin")
+    
+    // Determine cookie attributes based on origin and environment
+    var secure bool
+    var sameSite string
+    var partitioned string = ""
+    
+    if origin == "http://localhost:3000" {
+        // Local development
+        secure = false
+        sameSite = "Lax"
+    } else {
+        // Production or HTTPS origins
+        secure = true
+        sameSite = "None"
+        partitioned = "; Partitioned"
+    }
+    
     cookieStr := fmt.Sprintf(
-        "%s=%s; Path=/; HttpOnly; Secure; SameSite=None; Partitioned; Expires=%s",
+        "%s=%s; Path=/; HttpOnly",
         name,
         url.QueryEscape(value),
+    )
+    
+    if secure {
+        cookieStr += "; Secure"
+    }
+    
+    cookieStr += fmt.Sprintf("; SameSite=%s%s; Expires=%s",
+        sameSite,
+        partitioned,
         expires.UTC().Format(time.RFC1123),
     )
     
-    // Set cookie header
     ctx.Append("Set-Cookie", cookieStr)
+    
+    // Debug
+    fmt.Printf("Final cookie: %s\n", cookieStr)
     
     return nil
 }
